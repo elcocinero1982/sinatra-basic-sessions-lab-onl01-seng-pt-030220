7cbe2f418186f445d151102ea4729527b4774c47
jose luis <cortezluisjose@gmail.com>
2020-05-13 21:35:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require_relative 'config/environment'
 
-
+class App < Sinatra::Base
 	 configure do
    enable :sessions
 	 set :session_secret, "secret"
 	 end
 
-class App < Sinatra::Base
+get '/' do 
+  erb :index
+  
+end
 end
